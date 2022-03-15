97b04f46e9fb8ffbbd66d86fcac9dad6d4ad1860
Ammundhe <97632239+Ammundhe@users.noreply.github.com>
2022-03-15 15:02:16 -0700

    Header Component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import Header from "./component/Header";
+import "./App.css";
+
+const App=()=>{
+    return(
+        <div className="myApp">
+            <Header/>
+        </div>
+    )
+}
+export default App;
